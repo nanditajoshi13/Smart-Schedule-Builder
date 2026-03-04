ab655426fcf658996adc0b6b9c511b37efa85f05
nanditajoshi13 <nanditajoshi001@gmail.com>
2026-03-04 21:43:54 +0530

    The main exutable file

--- controller.py
+++ controller.py
@@ -7,15 +7,17 @@ from login_page import login_bp
 from register_page import register_bp
 from dashboard import dashboard_bp
 
-app = Flask(__name__, template_folder=os.path.dirname(os.path.abspath(__file__)))
+app = Flask(__name__)
 CORS(app)
 app.secret_key = "supersecretkey"
 
 DB_FILE = "users.db"
 
+
 def init_db():
     conn = sqlite3.connect(DB_FILE)
     cur = conn.cursor()
+
     cur.execute("""
     CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT, 
@@ -32,8 +34,10 @@ def init_db():
         goal TEXT
     )
     """)
+
     conn.commit()
     conn.close()
+
 
 init_db()
 
@@ -41,9 +45,11 @@ app.register_blueprint(login_bp, url_prefix="/login")
 app.register_blueprint(register_bp, url_prefix="/register")
 app.register_blueprint(dashboard_bp, url_prefix="/dashboard")
 
+
 @app.route("/")
 def home():
     return redirect(url_for("login.home"))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
