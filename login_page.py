f410296bcc414a0a6340fb4f96774deefc3ecff2
nanditajoshi13 <nanditajoshi001@gmail.com>
2026-03-04 21:59:39 +0530

    login page backend

--- login_page.py
+++ login_page.py
@@ -2,34 +2,37 @@ import os
 import sqlite3
 from flask import Blueprint, render_template, request, redirect, url_for, session, flash
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-login_bp = Blueprint("login", __name__, template_folder=BASE_DIR)
+login_bp = Blueprint("login", __name__)
 
 DB_FILE = "users.db"
+
 
 @login_bp.route("/", methods=["GET"])
 def home():
     return render_template("login_page.html")
 
+
 @login_bp.route("/login", methods=["POST"])
-def login():
+def login_user():
     uname = request.form.get("username")
     passw = request.form.get("password")
 
     if not os.path.exists(DB_FILE):
-        flash ("No users registered yet!")
+        flash("No users registered yet!")
         return redirect(url_for("login.home"))
 
-    
     conn = sqlite3.connect(DB_FILE)
+    conn.row_factory = sqlite3.Row
     cur = conn.cursor()
-    cur.execute("SELECT name, password FROM users WHERE username=?", (uname,))
+
+    cur.execute("SELECT * FROM users WHERE username=?", (uname,))
     row = cur.fetchone()
     conn.close()
 
-    if row and row[1] == passw:
-        session["name"] = row[0]
-        session["username"] = uname
+    if row and row["password"] == passw:
+        session["user_id"] = row["id"]
+        session["name"] = row["name"]
+        session["username"] = row["username"]
         return redirect(url_for("dashboard.dashboard_page"))
     else:
         flash("Invalid Username or Password!")
@@ -37,26 +40,31 @@ def login():
 
 
 @login_bp.route("/register", methods=["POST"])
-def register():
+def register_user():
     name = request.form.get("name")
     username = request.form.get("username")
     password = request.form.get("password")
 
     conn = sqlite3.connect(DB_FILE)
     cur = conn.cursor()
+
     try:
-        cur.execute("INSERT INTO users (name, username, password) VALUES (?, ?, ?)",
-                (name, username, password))
+        cur.execute(
+            "INSERT INTO users (name, username, password) VALUES (?, ?, ?)",
+            (name, username, password),
+        )
         conn.commit()
+        user_id = cur.lastrowid
+
     except sqlite3.IntegrityError:
-        flash("Username already exists. Please choose another one.")
+        flash("Username already exists.")
         conn.close()
         return redirect(url_for("login.home"))
-    finally: 
-        conn.close()
 
+    conn.close()
+
+    session["user_id"] = user_id
     session["name"] = name
     session["username"] = username
-    session["password"] = password
 
     return redirect(url_for("register.register_page"))
