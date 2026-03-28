3031d8c6f6ec68caf2e2f17605084acd507d21e5
nanditajoshi13 <nanditajoshi001@gmail.com>
2026-03-28 16:57:16 +0530

    switched to mysql

--- login_page.py
+++ login_page.py
@@ -1,34 +1,34 @@
 import os
-import sqlite3
+import mysql.connector
 from flask import Blueprint, render_template, request, redirect, url_for, session, flash
 
 login_bp = Blueprint("login", __name__)
 
-DB_FILE = "users.db"
-
+def get_db_connection():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="your_password", //write your mysql command line client password here
+        database="schedule_db"
+    )
 
 @login_bp.route("/", methods=["GET"])
 def home():
     return render_template("login_page.html")
-
 
 @login_bp.route("/login", methods=["POST"])
 def login_user():
     uname = request.form.get("username")
     passw = request.form.get("password")
 
-    if not os.path.exists(DB_FILE):
-        flash("No users registered yet!")
-        return redirect(url_for("login.home"))
-
-    conn = sqlite3.connect(DB_FILE)
-    conn.row_factory = sqlite3.Row
-    cur = conn.cursor()
-
-    cur.execute("SELECT * FROM users WHERE username=?", (uname,))
-    row = cur.fetchone()
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM users WHERE username=%s", (uname,))
+    row = cursor.fetchone()
+    cursor.close()
     conn.close()
 
+    
     if row and row["password"] == passw:
         session["user_id"] = row["id"]
         session["name"] = row["name"]
@@ -45,22 +45,24 @@ def register_user():
     username = request.form.get("username")
     password = request.form.get("password")
 
-    conn = sqlite3.connect(DB_FILE)
-    cur = conn.cursor()
+    conn =get_db_connection()
+    cursor = conn.cursor()
 
     try:
-        cur.execute(
-            "INSERT INTO users (name, username, password) VALUES (?, ?, ?)",
+        cursor.execute(
+            "INSERT INTO users (name, username, password) VALUES (%s, %s, %s)",
             (name, username, password),
         )
         conn.commit()
-        user_id = cur.lastrowid
+        user_id = cursor.lastrowid
 
-    except sqlite3.IntegrityError:
+    except mysql.connector.IntegrityError:
         flash("Username already exists.")
+        cursor.close()
         conn.close()
         return redirect(url_for("login.home"))
 
+    cursor.close()
     conn.close()
 
     session["user_id"] = user_id
